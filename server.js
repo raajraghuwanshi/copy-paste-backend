4ed9f0d772e8b3942a538a832db09c4f58e70130
raj raghuwanshi <rajraghuawnshi9479@gmail.com>
2026-05-07 22:32:22 +0530

    this is first commit and maybe the last

---
+++ server.js
@@ -0,0 +1,46 @@
+import express from 'express'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+
+const app = express()
+const httpServer = createServer(app)
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: "http://localhost:3000", // Ensure this matches your Next.js URL
+    }
+})
+
+// Store the latest content for each room in memory
+// In a production app, you'd use Redis or a Database
+const roomStates = {};
+
+
+io.on('connect', (socket) => {
+    socket.on('join', (roomId) => {
+        socket.join(roomId);
+
+        // If the room already has data, send it to the person who just joined
+        if (roomStates[roomId]) {
+            socket.emit('message', { message: roomStates[roomId] });
+        }
+    });
+
+    socket.on('send', ({ roomId, message }) => {
+        // Update the global state for this room
+        roomStates[roomId] = message;
+        // Sync to everyone else
+        socket.to(roomId).emit('message', { message });
+    });
+
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected')
+    })
+});
+
+
+
+httpServer.listen(4000, () => {
+    console.log('Server is running on port 4000')
+})
